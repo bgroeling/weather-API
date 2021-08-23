0efd48528322536a71ebd5a56fc62127ee237560
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-08-23 17:54:33 -0600

    additional js file

--- assets/js/fiveDay.js
+++ assets/js/fiveDay.js
@@ -1,28 +1,28 @@
 var searchbtn = document.querySelector(".search-btn");
 var input = document.querySelector(".city-input");
 var key = "01b3cd1976a0c8efe2a1cf86798399b9"
-var weatherToday = document.querySelector(".todays-weather")
 var weatherFuture = document.querySelector(".five-day-weather")
-
 
 searchbtn.addEventListener("click",function(){
     var cityvalue = input.value
     console.log(cityvalue)
-    //call function
-    todaysWeather(cityvalue)
+    // call function
+    futureWeather(cityvalue)
 })
-function todaysWeather(city){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`)
+function futureWeather(city){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=imperial`)
     
     .then(function(data){
         // returns data that we can read
         return data.json()
     }).then(function(data){
         console.log(data)
-        var temp = data.main.temp;
-        var humidity = data.main.humidity;
-        var windSpeed = data.wind.speed;
-        var cityName = data.name;
+        var temp = data.list.main.temp
+        var humidity = data.list.main.humidity
+        var windSpeed = data.wind.speed
+        var cityName = data.name
+        var lon = data.coord.long
+        var lat = data.coord.lat
 
         //h2 element for city name
         var h2 = document.createElement("h2")
